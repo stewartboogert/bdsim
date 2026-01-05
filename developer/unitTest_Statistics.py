ceadbcd3c04eca16e14ddd6bc0a52f2b17186638
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-01-05 16:18:43 +0000

    CI/CD: unit test plots

--- developer/unitTest_Statistics.py
+++ developer/unitTest_Statistics.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from tqdm import tqdm
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import pandas
+import numpy as np
 
 import pybdsim
 
@@ -9,13 +12,14 @@ def _generate_test_outputs(dirToScan) :
     files = list(root.rglob("*.root"))
     return files
 
-def generate(dirToScan):
+def generate(dirToScan, nFileLimit = 10000):
     files = _generate_test_outputs(dirToScan)
     filesSkipped = []
     filesNoRun = []
 
     dd = {}
     dd['index'] = []
+    dd['file_path'] = []
     dd['file_name'] = []
     dd['nGenerate'] = []
     dd['memoryUsageMb'] = []
@@ -37,6 +41,9 @@ def generate(dirToScan):
     dd['runDurationWall'] = []
 
     for i,f in tqdm(enumerate(files)) :
+
+        if i == nFileLimit :
+            break
 
         if "fodo-no-primaries.root" in str(f) or \
            "shower_cyl_sph.root" in str(f) :
@@ -60,13 +67,14 @@ def generate(dirToScan):
         es = p.get_event_summary()
 
         # output data
-        dd['file_name'].append(str(f))
+        dd['file_path'].append(str(f))
+        dd['file_name'].append(str(Path(f.name).stem))
 
         # index
         dd['index'].append(i)
 
         # number of events
-        dd['nGenerate'].append(o['nGenerate'])
+        dd['nGenerate'].append(o['nGenerate'][0])
 
         # memory usage
         dd['memoryUsageMb'].append(es['memoryUsageMb'].mean())
@@ -102,6 +110,43 @@ def generate(dirToScan):
 
     return pandas.DataFrame(dd)
 
-def plot(df) :
+def plot_all(df) :
+    ranges = list(range(0,len(df), 50))
+    ranges.append(len(df))
+    print(ranges)
+
+    for i,r in enumerate(ranges[0:-1]) :
+        print(i, ranges[i], ranges[i+1])
+        plot_page(df, i, ranges[i], ranges[i+1])
+
+
+    with PdfPages("plot_all.pdf") as pdf:
+        for fig_num in plt.get_fignums():
+            pdf.savefig(plt.figure(fig_num))
+
+
+def plot_selection(df, pathContains ="", fileContains = "") :
     pass
 
+def plot_page(df, ifigure=1, istart = 0, iend=50) :
+    a4size = np.array([8.27, 11.69])*0.7
+
+    plt.figure(figsize=tuple(a4size)) # A4
+
+    plt.subplot(1,4,1)
+    plt.barh(df['file_name'][istart:iend], df['nGenerate'][istart:iend])
+    # plt.xlim(0, df['nGenerate'].max())
+
+    plt.subplot(1,4,2)
+    plt.barh(df['file_name'][istart:iend].str.pad(width=30, side='right'),
+             df['runDurationWall'][istart:iend])
+    plt.gca().set_yticks([])
+    plt.xlim(0, df['runDurationWall'].max())
+
+    plt.subplot(1,4,3)
+    plt.barh(df['file_name'][istart:iend], df['memoryUsageMb'][istart:iend])
+    plt.gca().set_yticks([])
+    plt.xlim(0, df['memoryUsageMb'].max())
+
+
+    plt.tight_layout()
